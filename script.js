0e2352031899397e53dde010e280c83e7b226574
mike <malmonte827@gmail.comgit config --global user.name malmonte827dgit configqgit config --global user.email malmonte827@gmail.com>
2024-03-19 00:36:57 -0400

    final revision

--- script.js
+++ script.js
@@ -4,25 +4,27 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 
-inputField.onkeyup = search
+inputField.onkeyup = search;
 function search() {
 	let results = [];
 	let input = inputField.value;
 	
 	 if(input.length){
-		results = fruit.filter((value)=>{
-		 return value.toLowerCase().includes(input.toLowerCase())
-		})};
-	showSuggestions(results)
+		results = fruit.filter((value) => {
+		 return value.toLowerCase().includes(input.toLowerCase());
+		}
+	)};
+
+	showSuggestions(results);
 }
 	
 function showSuggestions(results) {
- const content = results.map((value)=>{
-	return '<li onclick = useSuggestion(this)>' + value + "</li>"
- })
+ const content = results.map((value) => {
+	return '<li onclick = useSuggestion(this)>' + value + "</li>";
+ });
  suggestions.innerHTML = content.join('');
 }
 
 function useSuggestion(value){
-	inputField.value = value.innerHTML 
+	inputField.value = value.innerHTML; 
 }
