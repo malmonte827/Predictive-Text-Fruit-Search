dda4391f4fe7627e67954dc8ae266a9d432be5a7
mike <malmonte827@gmail.comgit config --global user.name malmonte827dgit configqgit config --global user.email malmonte827@gmail.com>
2024-03-18 23:36:52 -0400

    finished javascript

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
-const input = document.querySelector('#fruit');
+const inputField = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
-// input.addEventListener('keydown', search);
 
-input.onkeyup = function() {
-	
+
+inputField.onkeyup = search
+function search() {
 	let results = [];
-	let inputs = input.value;
-	if(inputs.length){
-	 results = fruit.filter((value)=>{
-		 return value.toLowerCase().includes(inputs.toLowerCase())
-		});
-		console.log(results)
-}
+	let input = inputField.value;
+	
+	 if(input.length){
+		results = fruit.filter((value)=>{
+		 return value.toLowerCase().includes(input.toLowerCase())
+		})};
+	showSuggestions(results)
 }
 	
-
-function searchHandler(e) {
-	// const newLi = document.createElement('li')
-	// newLi.innerText = e;
-	// suggestions.append(newLi)
-	if(e.key === search){console.log('yes')}
+function showSuggestions(results) {
+ const content = results.map((value)=>{
+	return '<li onclick = useSuggestion(this)>' + value + "</li>"
+ })
+ suggestions.innerHTML = content.join('');
 }
 
-function showSuggestions(results, inputVal) {
-
+function useSuggestion(value){
+	inputField.value = value.innerHTML 
 }
-
-function useSuggestion(e) {
-console.log(e)
-}
-
-suggestions.addEventListener('click', useSuggestion);
