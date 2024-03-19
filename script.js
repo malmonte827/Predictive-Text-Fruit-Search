5920735d8af27d9d7054fa0206736067efb92f98
mike <malmonte827@gmail.comgit config --global user.name malmonte827dgit configqgit config --global user.email malmonte827@gmail.com>
2024-03-18 21:44:59 -0400

    onkeyup

--- script.js
+++ script.js
@@ -2,27 +2,34 @@ const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+// input.addEventListener('keydown', searchd);
 
-function search(str) {
+input.onkeyup = function() {
+	
 	let results = [];
-
-	// TODO
-
-	return results;
+	let inputs = input.value;
+	if(inputs.length){
+	 results = fruit.filter((value)=>{
+		 return value.toLowerCase().includes(inputs.toLowerCase())
+		});
+		console.log(results)
 }
+}
+	
 
 function searchHandler(e) {
-	// TODO
+	// const newLi = document.createElement('li')
+	// newLi.innerText = e;
+	// suggestions.append(newLi)
+	if(e.key === search){console.log('yes')}
 }
 
 function showSuggestions(results, inputVal) {
 
-	// TODO
 }
 
 function useSuggestion(e) {
-	// TODO
+console.log(e)
 }
 
-input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
